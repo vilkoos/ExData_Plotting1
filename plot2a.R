e5a0ead4b7a6a5121645b47e333b8ab63ab6d9a5
vilkoos <w.dijkhuis@home.nl>
2015-01-11 10:24:50 +0100

    now data read in the plot1/2/3/4.R files

---
+++ plot2a.R
@@ -0,0 +1,36 @@
+
+# if needed run script plot0_load_data.R to create the dataframe pow
+# str(pow) gives:
+'2880 obs. of  9 variables:
+$ Date                 : Date, format: "2007-02-01" "2007-02-01" "2007-02-01" "2007-02-01" ...
+$ DateTime             : POSIXct, format: "2007-02-01 00:00:00" "2007-02-01 00:01:00" "2007-02-01 00:02:00" "2007-02-01 00:03:00" ...
+$ Global_active_power  : num  0.326 0.326 0.324 0.324 0.322 0.32 0.32 0.32 0.32 0.236 ...
+$ Global_reactive_power: num  0.128 0.13 0.132 0.134 0.13 0.126 0.126 0.126 0.128 0 ...
+$ Voltage              : num  243 243 244 244 243 ...
+$ Global_intensity     : num  1.4 1.4 1.4 1.4 1.4 1.4 1.4 1.4 1.4 1 ...
+$ Sub_metering_1       : num  0 0 0 0 0 0 0 0 0 0 ...
+$ Sub_metering_2       : num  0 0 0 0 0 0 0 0 0 0 ...
+$ Sub_metering_3       : num  0 0 0 0 0 0 0 0 0 0 ...
+'
+
+#### make the plot ###############################
+plot(pow$DateTime, pow$Global_active_power, 
+     type= "l",
+     xlab = "",
+     ylab = "Global Active Power (kiloWatts)",
+     main = ""
+)
+
+#### copy to png #################################
+dev.copy(png, "plot2.png")
+dev.off() 
+
+
+'******** NOTE ***********************************
+to get the English names for days the set the setlocale var to Englisg with
+   Sys.setlocale("LC_TIME", "English_United Kingdom")
+set it back to Dutch with 
+  sys.setlocale("LC_TIME", "Dutch_Netherlands.1252")
+**************************************************' 
+
+
